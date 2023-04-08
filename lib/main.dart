740a2b333814d3550b9fc8f357d82d6057f8f6df
MohammeddFawzii <fawzimohamed@gmail.com>
2023-04-08 06:58:49 +0200

    UI finished

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,119 @@ class _PointsCounterState extends State<PointsCounter> {
             style: TextStyle(fontSize: 30),
           ),
         ),
+        body: Column(children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  const Text(
+                    "Team A",
+                    style: TextStyle(fontSize: 55),
+                  ),
+                  const Text(
+                    "0",
+                    style: TextStyle(fontSize: 200),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                    onPressed: () {},
+                    child: const Text(
+                      "Add 1 point",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                    onPressed: () {},
+                    child: const Text(
+                      "Add 2 point",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                    onPressed: () {},
+                    child: const Text(
+                      "Add 3 point",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ]),
+                const VerticalDivider(
+                  thickness: 2,
+                  indent: 70,
+                  endIndent: 70,
+                ),
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  const Text(
+                    "Team B",
+                    style: TextStyle(fontSize: 55),
+                  ),
+                  const Text(
+                    "0",
+                    style: TextStyle(fontSize: 200),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                    onPressed: () {},
+                    child: const Text(
+                      "Add 1 point",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                    onPressed: () {},
+                    child: const Text(
+                      "Add 2 point",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+                    onPressed: () {},
+                    child: const Text(
+                      "Add 3 point",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.brown),
+            onPressed: () {},
+            child: const Text(
+              "Reset",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 200,
+          )
+        ]),
       ),
     );
   }
